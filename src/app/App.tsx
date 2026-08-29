8c15a3aae5a66ee08df024949efd0621f281e04e
Maxim Murysov <murisov98@mail.ru>
2026-08-29 10:37:22 +0300

    feat(app): configure Redux store with RTK Query base api [TB-14]

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
